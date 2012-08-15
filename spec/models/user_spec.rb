966c3dc411b20583a757f62349544bea2d3b3d57
seung <selim50@gmail.com>
2012-08-14 19:34:15 -0700

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,12 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'spec_helper'
 
 describe User do
 
-  before do
+  #before_save { |user| user.email = email.downcase }
+  #before_save { |user| user.email = email.downcase }
+  #before_save { self.email.downcase! }
+  
+    before do
     @user = User.new(name: "Example User", email: "user@example.com", 
                      password: "foobar", password_confirmation: "foobar")
   end
@@ -32,18 +25,18 @@ describe User do
     before { @user.name = " " }
     it { should_not be_valid }
   end
-  
+ 
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+   
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
   
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
-  
-    describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -72,7 +65,17 @@ describe User do
     end
      it { should_not be_valid }
   end
-       
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+         
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
